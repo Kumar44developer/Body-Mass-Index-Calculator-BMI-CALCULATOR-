e1e5428d27e2a6516215a40e8a32add2410adc2b
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-08 12:35:55 +0530

    Parse weight value from form input as float

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ const gender = document.getElementById('gender').value;
 const age = parseInt(document.getElementById('age').value);
 const heightFeet = parseInt(document.getElementById('height-feet').value);
 const heightInches = parseInt(document.getElementById('height-inches').value);
+const weight = parseFloat(document.getElementById('weight').value);
 
