098a8fb2ce3cced0e80c759143702079a8295ed8
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-08 12:35:23 +0530

    Parse height in inches

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ document.getElementById("bmiForm").addEventListener('submit',function(e){
 const gender = document.getElementById('gender').value;
 const age = parseInt(document.getElementById('age').value);
 const heightFeet = parseInt(document.getElementById('height-feet').value);
+const heightInches = parseInt(document.getElementById('height-inches').value);
 
