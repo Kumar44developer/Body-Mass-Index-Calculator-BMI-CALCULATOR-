d5a39558d2fd65c7b92e52701dc06f9ba462402e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-09 14:38:58 +0530

    Added 'Over Weight' category for BMI in range 25–29.9

--- script.js
+++ script.js
@@ -18,5 +18,7 @@ if(bmi < 18.5){
 category = 'Under Weight';
  }else if (bmi >= 18.5 && bmi < 24.9){
 category = 'Normal Weight '
+}else if (bmi >= 25 && bmi < 29.9){
+category = 'Over Weight'  
     
 
