977293da0e1057718eb469bc81fc62a9e8d69492
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-08 21:38:38 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 document.getElementById("bmiForm").addEventListener('submit',function(e){
+
     e.preventDefault();
 
-const gender = document.getElementById('gender').value;
-const age = parseInt(document.getElementById('age').value);
-const heightFeet = parseInt(document.getElementById('height-feet').value);
-const heightInches = parseInt(document.getElementById('height-inches').value);
-const weight = parseFloat(document.getElementById('weight').value);
+    const gender = document.getElementById('gender').value;
+    const age = parseInt(document.getElementById('age').value);
+    const heightFeet = parseInt(document.getElementById('height-feet').value);
+    const heightInches = parseInt(document.getElementById('height-inches').value);
+    const weight = parseFloat(document.getElementById('weight').value);
 
-if(gender && age && heightFeet && heightInches && weight){
-const heightInMeters = ((heightFeet * 12) + heightInches) * 0.0254;  
-const bmi = weight / (heightInMeters * heightInMeters);
-const resultElement = document.getElementById("result");
+    if(gender && age && heightFeet && heightInches && weight){
 
-let category = '';
+        const heightInMeters = ((heightFeet * 12) + heightInches) * 0.0254;  
+        const bmi = weight / (heightInMeters * heightInMeters);
+        const resultElement = document.getElementById("result");
 
-if(bmi < 18.5){
-category = 'Under Weight';
- }else if (bmi >= 18.5 && bmi < 24.9){
-category = 'Normal Weight '
-}else if (bmi >= 25 && bmi < 29.9){
-category = 'Over Weight'  
- }else{
-category = 'Obese'
-}
+        let category = '';
 
-let resultMessage = 'Your BMI:' + bmi.toFixed(2) + '<br>';
-resultMessage += 'Category:' + category;
+        if(bmi < 18.5){
+            category = 'Under Weight';
+        }else if (bmi >= 18.5 && bmi < 24.9){
+            category = 'Normal Weight '
+        }else if (bmi >= 25 && bmi < 29.9){
+            category = 'Over Weight'    
+        }else{
+            category = 'Obese'
+        }
 
-resultElement.innerHTML = resultMessage;
-}   
-    
+        let resultMessage = 'Your BMI:' + bmi.toFixed(2) + '<br>';
+        resultMessage += 'Category:' + category;
 
+        resultElement.innerHTML = resultMessage;
+
+
+    }
+
+
+
+});
